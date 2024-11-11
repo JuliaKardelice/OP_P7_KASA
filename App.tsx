c4dbf2608b0e420de0dd0174fbab82577a33bc31
JuliaKardelice <julia.royer@live.fr>
2024-11-11 22:27:08 +0100

    Réorganisation du projet et quelques suppressions

--- App.tsx
+++ App.tsx
@@ -1,39 +1,44 @@
-import React from "react"
-import { Route, Routes, BrowserRouter as Router, NavLink } from 'react-router-dom'
-import logo from './assets/logo.png';
+import React from "react";
+import {
+  NavLink,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import "./App.scss";
+import logo from "./assets/logo.png";
 import { About } from "./pages/about/About";
-import { page404 } from "./pages/page404/page404";
-import './App.scss';
 
 export const App: React.FC = () => {
-
-
   return (
     <>
       <Router>
         <header>
           <img src={logo} alt="logo de Kasa" />
           <nav>
-            <NavLink className={({ isActive }) => isActive ? "active-link" : ""} to="/">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+              to="/"
+            >
               Accueil
             </NavLink>
-            <NavLink to="/" className={({ isActive }) => isActive ? "active-link" : ""} >
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
               A Propos
             </NavLink>
-
           </nav>
         </header>
-      <Routes>
-      
-      <Route path="/about" element={<About />} />
-      </Routes>
-      <Route path ="/page404" element= { page404 } />
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Route path="/page404" element={Page404} />
       </Router>
       <footer>
-      <img src={logo} alt="logo de Kasa" />
-      <p>© 2020 Kasa. All rights reserved</p>
+        <img src={logo} alt="logo de Kasa" />
+        <p>© 2020 Kasa. All rights reserved</p>
       </footer>
     </>
-  )
-}
-
+  );
+};
